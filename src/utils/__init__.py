eded0bb91a504a523d14edeab3626f51618f5658
Jorge Lopez <jorgelopezfueyo@gmail.com>
2019-03-20 14:43:04 +0100

    Single training working

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
-from .image_visualizations import imshow_rgb_d, imshow_unimodal
 from .constants import MEAN_RGB, STD_RGB, MEAN_DEPTH, STD_DEPTH, IMG_EXTENSIONS
+from .image_visualizations import imshow_rgb_d, imshow_unimodal
 from .rgb_d_transforms import Compose, CenterCrop, Resize, RandomCrop, RandomHorizontalFlip, RandomResizedCrop, \
     RandomVerticalFlip, Normalize, ToTensor
 from .dataset_utils import ImageFolder
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,10 +1,10 @@
 # DATSET CONSTANTS
 # Default ImageNet ILSRVC2012
-MEAN_RGB = [0.485, 0.456, 0.406]
-STD_RGB = [0.229, 0.224, 0.225]
+MEAN_RGB = (0.485, 0.456, 0.406)
+STD_RGB = (0.229, 0.224, 0.225)
 
-MEAN_DEPTH = [0.485, 0.456, 0.406]
-STD_DEPTH = [0.229, 0.224, 0.225]
+MEAN_DEPTH = (0.485, 0.456, 0.406)
+STD_DEPTH = (0.229, 0.224, 0.225)
 
 #img extensions
 IMG_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.ppm', '.bmp', '.pgm']
