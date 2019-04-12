dcd330f73ca21b7d2ea261719b74955069e24004
albamhp <albaherrerapalacio@gmail.com>
2019-04-12 13:54:21 +0200

    job

---
+++ jobs/job_notebook.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#SBATCH --job-name notebook
+#SBATCH --ntasks 4
+#SBATCH --mem 16G
+#SBATCH --partition mhigh,mlow
+#SBATCH --gres gpu:1
+#SBATCH --chdir /home/grupo06/m5-multimodal-encoder-decoder
+#SBATCH --output ../logs/%x_%u_%j.out
+
+source /home/grupo06/venv/bin/activate
+python jupyter-notebook --exp_name notebook${SLURM_JOB_ID}
