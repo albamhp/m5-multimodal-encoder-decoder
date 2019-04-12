a803aeb83c9370307c027f42bee05da50e507f93
albamhp <albaherrerapalacio@gmail.com>
2019-04-12 16:12:44 +0200

    Job fixed

--- jobs/job_notebook.sh
+++ jobs/job_notebook.sh
@@ -8,4 +8,4 @@
 #SBATCH --output ../logs/%x_%u_%j.out
 
 source /home/grupo06/venv/bin/activate
-python jupyter-notebook --no-browser --port=7810 notebook${SLURM_JOB_ID}
+jupyter notebook --no-browser --port=7810
